cc7d179c8e4ac3e9b791d1c71aa4cf215b9e543a
Niranjn M Nadiger <111889749+niranjanmnadiger@users.noreply.github.com>
2025-08-27 05:51:06 +0530

    learnt about arrow function

---
+++ ArrowFn.js
@@ -0,0 +1,21 @@
+// we learn about map filter and arrow fun 
+
+function sum(a,b){
+    return a+b;  
+}
+
+const sum = (a,b)=>{ 
+    return a+b;            //arrow fn
+
+}
+
+app.get('/',(req,res)=>{
+                                //arrow fn
+})
+
+app.get('/',function(req,res){
+
+})
+
+const ans = sum(1,2);
+console.log(ans);
