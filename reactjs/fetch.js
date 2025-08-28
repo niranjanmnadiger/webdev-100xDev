b6918a4d994961b6f5301162f6644dbc294149f8
Niranjan M Nadiger <111889749+niranjanmnadiger@users.noreply.github.com>
2025-08-28 19:37:49 +0530

    Update fetch.js with comments

--- reactjs/fetch.js
+++ reactjs/fetch.js
@@ -1,5 +1,5 @@
 function main() {
-  fetch("http://127.0.0.1:3000/") //local host link with the correct port number
+  fetch("http://127.0.0.1:3000/") //local host link with the port number
     .then(async (res) => {
       if (!res.ok) throw new Error(`HTTP ${res.status}`);
       const json = await res.json();
