1efc23fccdad17d29b82c7d53ccc0d31d6992d20
Niranjn M Nadiger <111889749+niranjanmnadiger@users.noreply.github.com>
2025-08-28 18:21:41 +0530

    react folder

---
+++ reactjs/axios.js
@@ -0,0 +1,16 @@
+function main() {
+  fetch("http://127.0.0.1:3000/")
+    .then(async (res) => {
+      if (!res.ok) throw new Error(`HTTP ${res.status}`);
+      const json = await res.json();
+
+      console.log("total kidneys:", json.numberofkidneys);
+      console.log("healthy kidneys:", json.numberofhealthykidneys);
+      console.log("unhealthy kidneys:", json.numberofunhealthykidneys);
+    })
+    .catch((err) => {
+      console.error("request failed:", err.message);
+    });
+}
+
+main();
