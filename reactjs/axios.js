e209cfd32d635963e11d990de13c1aa0ecfea9b1
Niranjn M Nadiger <111889749+niranjanmnadiger@users.noreply.github.com>
2025-08-30 10:29:37 +0530

    daily react practice

--- reactjs/axios.js
+++ reactjs/axios.js
@@ -1,16 +1,31 @@
-function main() {
-  fetch("http://127.0.0.1:3000/")
-    .then(async (res) => {
-      if (!res.ok) throw new Error(`HTTP ${res.status}`);
-      const json = await res.json();
+const axios = require("axios");
 
+/*
+async function main() {
+  const response = await fetch( "http://127.0.0.1:3000/");
+  const json = await response.json();
       console.log("total kidneys:", json.numberofkidneys);
       console.log("healthy kidneys:", json.numberofhealthykidneys);
       console.log("unhealthy kidneys:", json.numberofunhealthykidneys);
-    })
-    .catch((err) => {
-      console.error("request failed:", err.message);
-    });
+}
+*/
+
+async function main () {
+const response = await axios.get( "http://127.0.0.1:3000/");
+// response.data
+    console.log("total kidneys:", response.data.numberofkidneys);
+    console.log("healthy kidneys:", response.data.numberofhealthykidneys);
+    console.log("unhealthy kidneys:", response.data.numberofunhealthykidneys);
+}
+
+//post 
+async function posts(){
+  const response = await axios.post("https://webhook.site/4a9114c1-572d-4808-a40c-acd60d3fe3ee/");
+
+    console.log("total kidneys:", response.data);
+   
+  
 }
 
 main();
+posts();
