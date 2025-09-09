37645a33156d290a12542f9ae6332ff80ba4125f
Niranjn M Nadiger <111889749+niranjanmnadiger@users.noreply.github.com>
2025-09-09 20:30:21 +0530

    DailyDoseOfReact- understanding props

--- reactjs/reactApp/src/App.jsx
+++ reactjs/reactApp/src/App.jsx
@@ -1,97 +1,97 @@
-function App(){
+// Create a function component named App that will be rendered in the root element
+function App() {
+  // Array of post data (instead of hardcoded JSX)
+  const posts = [
+    {
+      name: "Bharat",
+      followerCount: 20,
+      time: "2m ago",
+      image:
+        "https://imgs.search.brave.com/F5OoHgfmJlOgbB00VWzyvvcxuQZk7JOPrCYV51JMmNU/rs:fit:860:0:0https://imgs.search.brave.com/F5OoHgfmJlOgbB00VWzyvvcxuQZk7JOPrCYV51JMmNU/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTI3/Mzc5NDUyL3Bob3Rv/L2tpdHR5LmpwZz9z/PTYxMng2MTImdz0w/Jms9MjAmYz13UWcw/TXdTcXJxZkkzWHdN/WGREVjhkY2RZMlNH/WVh2YndPdE12cUxX/UWpJPQ",
+      description:
+        "Want to know how to win big? Check out how these folks won $6000 in bounties.",
+    },
+    {
+      name: "Harkirat",
+      followerCount: 30,
+      time: "3m ago",
+      image:
+        "https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg",
+      description:
+        "How to get hired in 2024? I lost my job in 2023, this is the roadmap I followed to get hired in 2024.",
+    },
+    {
+      name: "Deepak",
+      followerCount: 50,
+      time: "18m ago",
+      image:
+        "https://imgs.search.brave.com/ZACv93qZO57A2RrexnAjJi9CTpejuyu2aIGeB9-2beA/rs:fit:860:0:0https://imgs.search.brave.com/ZACv93qZO57A2RrexnAjJi9CTpejuyu2aIGeB9-2beA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJhY2Nlc3Mu/Y29tL2Z1bGwvMTk0/OTM2My5qcGc",
+      description: "Here is the roadmap to become a full-stack developer in 2024.",
+    },
+  ];
 
-    // if you can see this function renders other two fucntions listed below <ProfileCard /> and <PostComponent />
-    return(
-        <div style={{background: "#dfe6e9", //wrapping every other component in a div 
-        height:"100vh", 
-        display: "flex", 
-        justifyContent: "center", 
-        padding: 10
-        }}>
-
-            <div style={{display: "flex"}}> 
-                <ProfileCard />  
-            
-            </div>
-
-            <div style={{
-                display: "flex",
-                //justifyContent: "center",
-                paddingLeft: 20,
-                paddingRight: 20
-                }}>
-                <div>
-                  <div><PostComponent /><br /></div> 
-                  <div><PostComponent /><br /></div> 
-                  <div><PostComponent /><br /></div> 
-                </div>
-            </div>   
-        </div>
-        
-    )
-}
-
-//this "style" is to do  styling for the PostComponent()
-const style = { width: 200, backgroundColor: "white", borderRadius: 10, borderColor : "grey", borderWidth: 1, padding: 20, alignItems: "center" } 
-//
-function PostComponent(){
-
-    return <div style={style}>
-        <div style={{display: "flex"}}>
-            <img src={"https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"} 
-            style = {{
-                width : 30,
-                height: 30,
-                borderRadius: 20
-            }}/>
-            <div style={{fontSize: 12, marginLeft: 10}}>
-                <b>
-                    100xDevs
-                </b>
-                <div>
-                    23,000 followers
-                </div>
-                <div>12 mins</div>
-                    
-            </div>
-        </div>
-        <div style={{fontSize: 14}}>what to learn web , click the link in description to know more!!</div>
-
-    </div>
-    
-}
-
-//this "profilecardStyle" is to do  styling for the  ProfileCard()
-const profilecardStyle = {
-  width: 200,
-  height: 200,
-  background: "#636e72",
-  borderRadius: 10,
-  borderWidth: 1,
-  display: "flex",          // enable flex
-  flexDirection: "column",  // stack children vertically
-  alignItems: "center",     // center horizontally
-  justifyContent: "center", // center vertically
-  textAlign: "center",      // center the text inside
-  padding: 10
-}
-
-function ProfileCard(){
   return (
-    <div style={profilecardStyle}>
-      <img 
-        src="https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg" 
-        style={{ width: 50, height: 50, borderRadius: "50%", marginBottom: 10 }}
-      />   
-      <div style={{ fontSize: 14 }}><b>Niranjan M Nadiger</b></div>
-      <div style={{ fontSize: 12 }}>Frontend Developer</div>
-      <div style={{ fontSize: 10 }}>
-        aspiring fullstack dev with interest in web3 tech etc etc etc
-      </div>      
+    <div style={appStyle}>
+      <div style={containerStyle}>
+        <div>
+          {/* Mapping through posts array and passing props dynamically */}
+          {posts.map((post, index) => (
+            <PostComponent key={index} {...post} />
+          ))}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
- 
+// Reusable Style Objects
+const appStyle = {
+  backgroundColor: "#dfe6e9",
+  height: "100vh",
+};
 
-export default App
+const containerStyle = {
+  display: "flex",
+  justifyContent: "center",
+};
+
+const postStyle = {
+  width: 500,
+  backgroundColor: "white",
+  borderRadius: 10,
+  borderColor: "gray",
+  borderWidth: 1,
+  padding: 20,
+  margin: 10,
+};
+
+// Post Component with props -- eslint-disable-next-line react/prop-types
+function PostComponent({ name, followerCount, time, image, description }) {
+  return (
+    <div style={postStyle}>
+      <div style={{ display: "flex" }}>
+        <img
+          src={image}
+          alt="User"
+          style={{ width: 40, height: 40, borderRadius: 20 }}
+        />
+        <div style={{ fontSize: 14, marginLeft: 10 }}>
+          <b>{name}</b>
+          <div>{followerCount} followers</div>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <div>{time}</div>
+            <img
+              src="https://imgs.search.brave.com/HR4XL7q_XpiQSqNwAl17jYkdNBrGw-fKdXxJlCr-uls/rs:fit:860:0:0https://imgs.search.brave.com/HR4XL7q_XpiQSqNwAl17jYkdNBrGw-fKdXxJlCr-uls/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTM3/MDc3ODM3Ni9waG90/by9jbG9jay02LW9j/bG9jay5qcGc_cz02/MTJ4NjEyJnc9MCZr/PTIwJmM9VTZKUXI5/UFdPYmVuY2lOMHVj/QkgycXJFeWd5WlY4/VUdDem1UQ1pVRnVE/cz0"
+              alt="Clock"
+              style={{ width: 20, height: 15, marginLeft: 5 }}
+            />
+          </div>
+        </div>
+      </div>
+      <div style={{ fontSize: 14 }}>{description}</div>
+    </div>
+  );
+}
+
+// Export App Component
+export default App;
