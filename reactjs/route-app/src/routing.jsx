ec266572043b77c5cfe08c9ab719ba52f0360833
Niranjn M Nadiger <111889749+niranjanmnadiger@users.noreply.github.com>
2025-09-16 11:16:15 +0530

    layouts navigation and routes in react

---
+++ reactjs/route-app/src/routing.jsx
@@ -0,0 +1,91 @@
+//import { useState } from 'react'
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route, Link, useNavigate } from "react-router-dom";
+
+
+function App() {
+  
+
+  return(
+    //<a></a> is a bad way to navigate to diffrent parts of Single page application - coz this will fetch the whole page
+    // instead use the Link tag -- you cannot use links outside the  <BrowserRouter>
+    <div>
+      
+     <BrowserRouter>
+        <Link to="/">LANDING PAGE</Link> | 
+        <Link to="/neet/class-11-online-coaching">CLASS 11</Link> | 
+        <Link to="/neet/class-12-online-coaching">CLASS 12</Link> --  this thing would appear on all the pages
+
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/neet/class-11-online-coaching" element={<Class11Program />} />
+          <Route path="/neet/class-12-online-coaching" element={<Class12Program />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+     </BrowserRouter>
+    </div>
+  )
+
+}
+
+
+function LandingPage(){
+
+
+  return(
+
+  // even this <Link> works, which will navigate you  to the desired location which is set inside the link tag
+    <div>
+      you are in the landing page
+      <Link to="/neet/class-11-online-coaching"><button>CLASS 11</button></Link>
+      <Link to="/neet/class-12-online-coaching"><button>CLASS 12</button></Link>
+    </div>
+  )
+}
+
+function Class11Program(){
+  const navigate = useNavigate();
+
+  useEffect(()=>{ // this will push back the user or send back the user on the landing page after 5 seconds just like payment gateway!!!
+     setTimeout(redirectUser,5000)
+  })
+
+  function redirectUser(){
+    navigate("/")
+  }
+
+  return(
+    <div>
+      neet program for 11th class
+    </div>
+  )
+}
+
+function Class12Program(){
+     const navigate = useNavigate();
+
+     function redirectUser(){
+        navigate("/");
+     }
+
+  return(
+    <div>
+      neet program for 12th class
+      <br />
+      <button onClick={redirectUser}>click for home page</button>
+    </div>
+  )
+
+}
+
+function ErrorPage(){ // this one for error page - 404 page, when people try to hit unknown routes - and "*" inside does that all 
+
+  return(
+    <div>
+      error page , please go back 
+      <br />
+      <Link to="/"><button>Home page</button></Link>
+    </div>
+  )
+}
+export default App
