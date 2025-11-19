978b3040f6308434ba7b50680067c50b2b7670d6
Niranjn M Nadiger <111889749+niranjanmnadiger@users.noreply.github.com>
2025-11-19 19:11:03 +0530

    working with tailwind and built one OTP component

---
+++ reactjs/tailwindCSS/src/App.jsx
@@ -0,0 +1,21 @@
+
+function App() {
+
+
+  return (
+    <div className="grid  sm:grid-cols-12 grid-cols-1  ">
+      <div className="col-span-1 sm:col-span-4 bg-yellow-400 px-10 py-10">
+        class 1
+      </div>
+      <div className="col-span-1 sm:col-span-4 bg-red-400 px-10 py-10">
+        class 2
+      </div>
+      <div className="col-span-1 sm:col-span-4 bg-blue-400 px-10 py-10">
+        class 3
+
+      </div>
+
+    </div>);
+}
+
+export default App
---
+++ reactjs/tailwindCSS/src/grid.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+    return (
+        <div className="grid grid-cols-12">
+            <div className="text-blue-300 col-span-3 bg-blue-400">hello there</div>
+            <div className="col-span-2 bg-green-700">class 1</div>
+            <div className="col-span-6 bg-red-300">class 2</div>
+            <div className="col-span-1 bg-stone-300">class 3</div>
+        </div>
+    );
+}
+
+export default App
