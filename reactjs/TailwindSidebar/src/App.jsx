28934bdbba9aefa0122ef0b5611741101816a100
Niranjn M Nadiger <111889749+niranjanmnadiger@users.noreply.github.com>
2025-11-24 16:08:48 +0530

    daily psuh: day 1 - tailwindcss sidebar

--- reactjs/TailwindSidebar/src/App.jsx
+++ reactjs/TailwindSidebar/src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//import { useMemo, useState } from "react";
+import {
+  ThemeProvider,
 
-function App() {
-  const [count, setCount] = useState(0)
+} from "@mui/material/styles";
+import {
+  CssBaseline,
+  Card,
+  CardContent,
+  CardHeader,
+  Typography,
+  Button,
+  Box,
+} from "@mui/material";
+
+const App = () => {
+  // //const [mode, setMode] = useState("light");
+
+  // // create theme based on mode
+  // const theme = useMemo(
+  //   () =>
+  //     createTheme({
+  //       palette: {
+  //         mode, // "light" or "dark"
+  //       },
+  //     }),
+  //   [mode]
+  // );
+
+  // const toggleMode = () => {
+  //   setMode((prev) => (prev === "light" ? "dark" : "light"));
+  // };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+    <div>
+
+
+      <div class="grid grid-cols-2 sm:grid-cols-3">
+
+      </div>
+
+    </div>
+  );
+};
+
+export default App;
