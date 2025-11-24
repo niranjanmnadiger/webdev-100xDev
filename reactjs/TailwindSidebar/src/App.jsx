599e2522f5679ab18c5561d45fe0d365a735fe47
Niranjn M Nadiger <111889749+niranjanmnadiger@users.noreply.github.com>
2025-11-24 17:42:05 +0530

    basic side bar

--- reactjs/TailwindSidebar/src/App.jsx
+++ reactjs/TailwindSidebar/src/App.jsx
@@ -1,47 +1,18 @@
-//import { useMemo, useState } from "react";
-import {
-  ThemeProvider,
-
-} from "@mui/material/styles";
-import {
-  CssBaseline,
-  Card,
-  CardContent,
-  CardHeader,
-  Typography,
-  Button,
-  Box,
-} from "@mui/material";
+import { SideBar } from "./Components/Sidebar"
 
 const App = () => {
-  // //const [mode, setMode] = useState("light");
-
-  // // create theme based on mode
-  // const theme = useMemo(
-  //   () =>
-  //     createTheme({
-  //       palette: {
-  //         mode, // "light" or "dark"
-  //       },
-  //     }),
-  //   [mode]
-  // );
-
-  // const toggleMode = () => {
-  //   setMode((prev) => (prev === "light" ? "dark" : "light"));
-  // };
 
   return (
-
-    <div>
-
-
-      <div class="grid grid-cols-2 sm:grid-cols-3">
-
+    <>
+      <div>
+        <SideBar></SideBar>
       </div>
 
-    </div>
-  );
-};
 
-export default App;
+    </>
+
+  )
+}
+
+
+export default App
