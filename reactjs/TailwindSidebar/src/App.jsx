e40d6fffea1d3849273857d00b96856b14ef8ddf
Niranjn M Nadiger <111889749+niranjanmnadiger@users.noreply.github.com>
2025-11-24 19:56:19 +0530

    day:1 - last push for the day

--- reactjs/TailwindSidebar/src/App.jsx
+++ reactjs/TailwindSidebar/src/App.jsx
@@ -1,16 +1,68 @@
-import { SideBar } from "./Components/Sidebar"
+
+import { HomeIcon } from "./icons/Home"
+import { SidebarIcon } from "./icons/sidebar"
 
 const App = () => {
 
   return (
-    <>
-      <div>
-        <SideBar></SideBar>
+    <div className="flex">
+      <Sidebar />
+      <Content />
+
+    </div>
+
+
+
+
+
+  )
+}
+
+function Sidebar() {
+
+  return (
+
+    <div className="bg-amber-200 w-60 h-screen">
+      <SidebarIcon />
+
+      <HomeIcon />
+
+
+
+    </div>
+  )
+}
+
+
+
+
+function Content() {
+
+  return (
+
+    <div className="w-screen h-screen mx-1">
+      <div className="bg-black text-white py-7"> Fight for Your fairytale! </div>
+
+      <div className="bg-blue-100 grid grid-cols-12 gap-3">
+
+        <div className="col-span-2 bg-red-300 py-20 -translate-y-5 ">
+          Image and info
+        </div>
+
+        <div className="col-span-6 bg-amber-400 py-20">
+          meeting shedules
+        </div>
+
+        <div className="col-span-4 bg-blue-500">
+          extras
+        </div>
+
+
+
       </div>
 
 
-    </>
-
+    </div>
   )
 }
 
