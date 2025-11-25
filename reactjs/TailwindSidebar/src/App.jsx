9001dd73ef318d927c825df2714ede693734ee3a
Niranjn M Nadiger <111889749+niranjanmnadiger@users.noreply.github.com>
2025-11-25 18:47:12 +0530

    daily push:2 - tried to understand responsiveness + stiching the components together to make a website

--- reactjs/TailwindSidebar/src/App.jsx
+++ reactjs/TailwindSidebar/src/App.jsx
@@ -1,36 +1,78 @@
 
+import { useState } from "react"
 import { HomeIcon } from "./icons/Home"
 import { SidebarIcon } from "./icons/sidebar"
 
 const App = () => {
 
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+
   return (
     <div className="flex">
-      <Sidebar />
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
       <Content />
 
     </div>
-
-
-
-
-
   )
 }
 
-function Sidebar() {
+function Sidebar({ sidebarOpen, setSidebarOpen }) {
 
-  return (
+  if (sidebarOpen) {
+    return (
 
-    <div className="bg-amber-200 w-60 h-screen">
-      <SidebarIcon />
+      <div className="bg-amber-200 w-60 h-screen rounded-xl">
 
-      <HomeIcon />
+        <div>
+
+          <div className="cursor-pointer hover:bg-amber-600 transform-all delay-100 duration-2000" onClick={() => { setSidebarOpen(!sidebarOpen) }}>
+            <SidebarIcon />
+          </div>
+
+
+          <div className="space-y-3 ">
+            <div className="flex text-xl border rounded-md ">
+
+              <HomeIcon />
+              Home
+
+            </div>
+            <div className="flex text-xl border rounded-md ">
+
+              <HomeIcon />
+              contact
+
+            </div>
+            <div className="flex text-xl border rounded-md ">
+
+              <HomeIcon />
+              projects
+
+            </div>
+            <div className="flex text-xl border rounded-md ">
+
+              <HomeIcon />
+              email
+
+            </div>
+          </div>
+        </div>
 
 
 
-    </div>
-  )
+      </div>
+    )
+  } else {
+    return (
+
+      <div className="cursor-pointer bg-amber-200 hover:bg-amber-600 transform-all delay-100 duration-2000" onClick={() => { setSidebarOpen(!sidebarOpen) }}>
+        <SidebarIcon />
+      </div>
+
+    )
+  }
+
+
 }
 
 
@@ -40,20 +82,20 @@ function Content() {
 
   return (
 
-    <div className="w-screen h-screen mx-1">
-      <div className="bg-black text-white py-7"> Fight for Your fairytale! </div>
+    <div className="w-screen h-screen">
+      <div className="bg-black rounded-md text-white h-47 text-3xl flex items-center justify-center hidden  sm:block"> Fight for Your fairytale! </div>
 
-      <div className="bg-blue-100 grid grid-cols-12 gap-3">
+      <div className=" grid grid-cols-12 gap-8 p-5">
 
-        <div className="col-span-2 bg-red-300 py-20 -translate-y-5 ">
+        <div className="shadow-xl sm:col-span-3 col-span-12 bg-red-300 h-70 -translate-y-10 rounded-xl hidden  sm:block ">
           Image and info
         </div>
 
-        <div className="col-span-6 bg-amber-400 py-20">
+        <div className="shadow-xl sm:col-span-5 col-span-12 h-70 bg-amber-400 rounded-xl ">
           meeting shedules
         </div>
 
-        <div className="col-span-4 bg-blue-500">
+        <div className="shadow-xl sm:col-span-4 col-span-12 h-70 bg-blue-500 rounded-xl ">
           extras
         </div>
 
