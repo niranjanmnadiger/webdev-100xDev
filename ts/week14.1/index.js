34435bc53a6574f16dc0c8f1c64cda7fecac9b55
Niranjn M Nadiger <111889749+niranjanmnadiger@users.noreply.github.com>
2026-02-02 19:12:09 +0530

    daily push

--- ts/week14.1/index.js
+++ ts/week14.1/index.js
@@ -4,6 +4,6 @@ function delay(fn) {
     setTimeout(fn, 3000);
 }
 delay(() => {
-    console.log("hello there after 3 seconds");
+    console.log("after 3 seconds");
 });
 //# sourceMappingURL=index.js.map
--- ts/week14.1/index.ts
+++ ts/week14.1/index.ts
@@ -1,7 +1,13 @@
-function delay(fn: () => void) {
+function delay(fn: () => void)
+//function delay(fn: any ) --> this will also workout but will lose out the whole idea of TS
+// its better to mention particular types rather than any type 
+{
     setTimeout(fn, 3000);
+
 }
 
-delay(() => {
-    console.log("hello there after 3 seconds")
-})
+delay(
+    () => {
+        console.log("after 3 seconds")
+    }
+)
