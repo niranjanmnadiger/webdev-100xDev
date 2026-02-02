650c7a1501fc63b9c56b86aedea425d67d067d7a
Niranjn M Nadiger <111889749+niranjanmnadiger@users.noreply.github.com>
2026-02-02 19:59:56 +0530

    daily push

---
+++ ts/week14.1/age.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function age(x) {
+    if (x > 18) {
+        console.log("leagal");
+    }
+    else {
+        console.log("illegal");
+    }
+}
+age(17);
+//# sourceMappingURL=age.js.map
