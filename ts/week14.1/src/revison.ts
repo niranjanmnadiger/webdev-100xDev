10da83d48cb418e1da3d4fe28964a4719875b858
Niranjn M Nadiger <111889749+niranjanmnadiger@users.noreply.github.com>
2026-02-04 13:18:55 +0530

    morning push

---
+++ ts/week14.1/src/revison.ts
@@ -0,0 +1,12 @@
+function sum(a: number, b: number): number {
+    return a + b;
+
+}
+
+function isTrue(age: number): boolean {
+    if (age < 18) {
+        return true;
+    } else {
+        return false;
+    }
+}
