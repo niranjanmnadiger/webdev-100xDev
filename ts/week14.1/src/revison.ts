69ab8b5673b9e212605e518797d644ba480aa5ac
Niranjn M Nadiger <111889749+niranjanmnadiger@users.noreply.github.com>
2026-02-25 20:03:34 +0530

    daily push

--- ts/week14.1/src/revison.ts
+++ ts/week14.1/src/revison.ts
@@ -1,18 +1,30 @@
-function maxValue(arr: number[]): number {
-    if (arr.length === 0) {
-        throw new Error("Array cannot be empty");
-    }
-
-    let max = arr[0]; // ✅ now TS knows arr[0] exists
-
-    for (let i = 1; i < arr.length; i++) {
-        const current = arr[i]; // current is a number
-        if (current > max) {
-            max = current;
-        }
-    }
-
-    return max;
+type User = {
+    name: string,
+    age: number,
+    address: string
 }
 
-console.log(maxValue([1, 2, 3]));
+interface Compnay {
+    name: string,
+    employeeID: number,
+    dept: string
+}
+
+type Union = User | Compnay
+type Intersection = User & Compnay
+
+let random: Union = { // union must have all the feild of first type and can have some or all the feilds of second type
+    name: "niri",
+    // age: 11,
+    address: "BLR",
+    dept: "TEch"
+
+}
+
+let random2: Intersection = {   //intersection must have all the feilds of all the types or interfacex
+    name: "niri",
+    age: 12,
+    address: "hello",
+    employeeID: 1122,
+    dept: "CS"
+}
